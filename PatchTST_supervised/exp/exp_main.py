ea68ac9fcc4519a0b4019b3b315f72b63d89bf57
Anton <104712265+m6129@users.noreply.github.com>
2024-04-20 16:18:57 +0300

    Update exp_main.py

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -1,7 +1,7 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
 from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST
-from utils.tools import EarlyStopping, adjust_learning_rate, visual, visual3 test_params_flop
+from utils.tools import EarlyStopping, adjust_learning_rate, visual, visual3, test_params_flop
 from utils.metrics import metric
 
 import numpy as np
