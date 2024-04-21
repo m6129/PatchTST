47c9b1dd17f4ddddc27657e945a780834ed71ad7
Anton <104712265+m6129@users.noreply.github.com>
2024-04-21 14:28:15 +0300

    Update illness.sh

--- PatchTST_supervised/scripts/PatchTST/illness.sh
+++ PatchTST_supervised/scripts/PatchTST/illness.sh
@@ -16,7 +16,7 @@ data_name=custom
 random_seed=2021
 for pred_len in 24 36 48 60
 do
-    python -u run_longExp.py \
+    python -u run_longExp_illness.py \
       --random_seed $random_seed \
       --is_training 1 \
       --root_path $root_path_name \
