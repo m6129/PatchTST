cecddbc6859a42ac3eebfe581f2ef1992bc80c83
Anton <104712265+m6129@users.noreply.github.com>
2024-05-04 12:13:54 +0300

    del stats_models

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST, Stat_models
+from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
@@ -32,7 +32,7 @@ class Exp_Main(Exp_Basic):
             'NLinear': NLinear,
             'Linear': Linear,
             'PatchTST': PatchTST,
-            'Stat_models': Stat_models
+        
         }
         model = model_dict[self.args.model].Model(self.args).float()
 
